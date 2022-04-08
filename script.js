90e1688ce4dc0eb91f7b4de1e49f4fd894501294
KokeHaus <austinbcw@gmail.com>
2022-04-08 11:16:07 -0500

    wow commits

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 var select =document.getElementById('dropdownlength');
 var charselect =document.getElementById('dropdownchars');
-var pwlength = select.options[select.selectedIndex].value;
+// var pwlength = select.options[select.selectedIndex].value;
 var charchoice = charselect.options[charselect.selectedIndex].value;
 // Write password to the #password input
 function writePassword() {
@@ -21,15 +21,32 @@ function generatePassword(){
   var password="";
   var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var select =document.getElementById('dropdownlength');
-var pwlength = select.options[select.selectedIndex].value;
+var pwlength = $('#charInput').val();
+console.log(pwlength)
 var charselect =document.getElementById('dropdownchars');
 var charchoice = charselect.options[charselect.selectedIndex].value;
 if(charchoice === "lowercase")
-  chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  chars = "abcdefghijklmnopqrstuvwxyz";
+if(charchoice === "lowercase+numbers")
+  chars ="0123456789abcdefghijklmnopqrstuvwxyz";
 if(charchoice === "capital")
-  chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if(charchoice === "capital+numbers")
+  chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if(charchoice === "capital+lowercase+numbers")
+  chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if(charchoice === "special characters and lowercase")
+  chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+if(charchoice === "special characters and lowercase and numbers")
+  chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
  if(charchoice === "special characters")
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   chars = "!@#$%^&*()";
+if(charchoice === "special characters and numbers")
+  chars = "0123456789!@#$%^&*()"
+if(charchoice === "special characters and lowercase and numbers and capitals")
+  chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+if(charchoice === "special characters and capitals and numbers")
+  chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
   var passwordLength = pwlength;
   for (var i = 0; i <= passwordLength-1; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
@@ -37,3 +54,5 @@ if(charchoice === "capital")
    }
    return password;
 }
+
+
